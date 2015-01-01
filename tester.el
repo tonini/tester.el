3925f51fe480af67cca0a66e3cc229f98a0f244c
Samuel Tonini <tonini.samuel@gmail.com>
2015-01-01 21:23:26 +0100

    add docstrings

--- tester.el
+++ tester.el
@@ -34,12 +34,15 @@
   "Hash table to store information about the last test run.")
 
 (defun tester-init-test-run (function match)
+  "Initialize the FUNCTION which will be used when `tester-run-test-file' is called.
+The FUNCTION will just be stored when the current `buffer-file-name' matches with MATCH."
   (puthash "match" match tester--storage)
   (if (and buffer-file-name (string-match match buffer-file-name))
       (puthash "test-run-function" function tester--storage)
     (puthash "test-run-function" nil tester--storage)))
 
 (defun tester-init-test-suite-run (function)
+  "Initialize the FUNCTION which will be used when `tester-run-test-suite' is called."
   (puthash "test-suite-run-function" function tester--storage))
 
 (defun tester--store-test-file-run ()
@@ -47,29 +50,38 @@
   (puthash "last-test-function" (gethash "test-run-function" tester--storage) tester--storage))
 
 (defun tester--store-test-file-run-p ()
+  "Store the informations from the last test file run."
   (and (gethash "last-test-file" tester--storage)
        (gethash "last-test-function" tester--storage)))
 
 (defun tester--test-run-function ()
+  "Return the current stored function which run the test file."
   (gethash "test-run-function" tester--storage))
 
 (defun tester--test-suite-run-function ()
+  "Return the current stored function which run the test suite."
   (gethash "test-suite-run-function" tester--storage))
 
 (defun tester--last-test-function ()
+  "Return the last stored function which run the test file."
   (gethash "last-test-function" tester--storage))
 
 (defun tester--last-test-suite-function ()
+  "Return the last stored function which run the test suite."
   (gethash "last-test-suite-function" tester--storage))
 
 (defun tester--last-test-file ()
+  "Return the last stored test file."
   (gethash "last-test-file" tester--storage))
 
 (defun tester--buffer-test-file-p ()
+  "Return t if the current file in buffer match as test file.
+Otherwise return nil."
   (and buffer-file-name
        (string-match (gethash "match" tester--storage) buffer-file-name)))
 
 (defun tester-run-test-file ()
+  "Run the current test file."
   (interactive)
   (cond ((and (tester--test-run-function)
               (tester--buffer-test-file-p))
@@ -78,9 +90,10 @@
         ((tester--store-test-file-run-p)
          (funcall (tester--last-test-function) (tester--last-test-file)))
         (t
-         (message "Please setup a function for running a test."))))
+         (message "Please setup a function for running a test file."))))
 
 (defun tester-run-test-suite ()
+  "Run the whole test suite."
   (interactive)
   (cond ((tester--test-suite-run-function)
          (puthash "last-test-suite-function" (tester--test-suite-run-function) tester--storage)
